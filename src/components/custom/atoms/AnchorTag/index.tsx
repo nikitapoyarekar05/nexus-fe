77e3d4f2bd446fea7b34def84a495aa55e89f5ec
Nikita Deepak Poyrekar <nikita.poyarekar5@gmail.com>
2025-06-06 10:38:14 +0530

    Remove unused file

--- src/components/custom/atoms/AnchorTag/index.tsx
+++ src/components/custom/atoms/AnchorTag/index.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link";
 
-import styles from "./AnchorTag.module.scss";
-
 interface AnchorTagProps {
   label: string;
   url: string;
@@ -14,7 +12,7 @@ const AnchorTag = (props: AnchorTagProps) => {
   const { id, label, url, dataTestId, customClass } = props;
   return (
     <Link
-      className={`${styles.anchorTag} ${customClass ? customClass : ""}`}
+      className={`${customClass ? customClass : ""}`}
       id={id}
       data-testid={dataTestId ?? id}
       href={url}
