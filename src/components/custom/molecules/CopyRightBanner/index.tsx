fc15dc1770398643c39ebf2a991a33d4afc96f9c
Nikita Deepak Poyrekar <nikita.poyarekar5@gmail.com>
2025-06-10 03:47:08 +0530

    Add design credits

--- src/components/custom/molecules/CopyRightBanner/index.tsx
+++ src/components/custom/molecules/CopyRightBanner/index.tsx
@@ -23,7 +23,17 @@ const CopyRightBanner = () => {
   return (
     <footer className="flex flex-col md:flex-row items-center justify-between py-4 bg-white border-t text-center md:text-left gap-4 md:gap-0">
       <div className="text-sm text-gray-500">
-        &copy; {new Date().getFullYear()} Neighbourly. All rights reserved.
+        &copy; {new Date().getFullYear()} Neighbourly. All rights reserved. UI
+        inspired by a design from{" "}
+        <a
+          href="https://www.figma.com/design/4XB2IJ2P5mjzHsujnBl0hl/-FREE--Estatery---Real-Estate-SaaS-Web-and-Mobile-UI-Kit--Community-?node-id=0-1&p=f&t=lCExpjbD2X2i7Rnn-0"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline text-primary"
+        >
+          Figma Community
+        </a>
+        .
       </div>
       <div className="flex items-center justify-center gap-3">
         {socialLinks.map(({ href, icon, label }) => (
